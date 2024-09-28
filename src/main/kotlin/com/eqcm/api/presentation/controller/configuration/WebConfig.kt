4a8c382f649e5445605ff8d83e9944e92e793331
JisuNa <kuu2002@naver.com>
2024-09-28 22:28:38 +0900

    eqCM-14 Fix cors error

---
+++ src/main/kotlin/com/eqcm/api/presentation/controller/configuration/WebConfig.kt
@@ -0,0 +1,14 @@
+package com.eqcm.api.presentation.controller.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig: WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*")
+            .allowedMethods("POST", "PUT", "GET", "DELETE")
+    }
+}
