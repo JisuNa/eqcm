f06027ad787fbeb82219e42dbd804d97a73af7f3
Jisu Na <kuu2002@naver.com>
2024-10-01 21:40:31 +0900

    eqCM-16 Fix cors error localhost:3000 (#17)

--- src/main/kotlin/com/eqcm/api/presentation/controller/configuration/WebConfig.kt
+++ src/main/kotlin/com/eqcm/api/presentation/controller/configuration/WebConfig.kt
@@ -7,14 +7,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 @Configuration
 class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
-        registry.addMapping("/**")
-            .allowedOrigins("*")
+        registry.addMapping(ALLOWED_MAPPING)
+            .allowedOrigins(ALLOWED_ORIGINS)
             .allowedHeaders(ALLOWED_HEADERS)
             .allowedMethods(ALLOWED_METHODS)
     }
 
     companion object {
+        const val ALLOWED_MAPPING = "/**"
         const val ALLOWED_METHODS = "POST, PUT, GET, DELETE, OPTIONS"
-        const val ALLOWED_HEADERS = "Content-Type, Authorization"
+        const val ALLOWED_ORIGINS = "http://localhost:3000"
+        const val ALLOWED_HEADERS = "*"
     }
 }
