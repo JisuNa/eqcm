56bb7bbd6f29c609fd6b6e58affb568c80e5fcd5
JisuNa <kuu2002@naver.com>
2024-10-02 09:08:03 +0900

    Apply Comment: Handle HttpMessageNotReadableException

--- src/main/kotlin/com/eqcm/api/presentation/configuration/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/eqcm/api/presentation/configuration/GlobalExceptionHandler.kt
@@ -3,6 +3,8 @@ package com.eqcm.api.presentation.configuration
 import com.eqcm.api.application.common.GlobalResponseType
 import com.eqcm.api.application.exception.BaseException
 import com.eqcm.api.presentation.common.response.ErrorResponse
+import com.fasterxml.jackson.databind.exc.MismatchedInputException
+import org.springframework.http.converter.HttpMessageNotReadableException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -19,6 +21,20 @@ class GlobalExceptionHandler {
     @ExceptionHandler(HandlerMethodValidationException::class)
     fun handleHandlerMethodValidationException(e: HandlerMethodValidationException): ErrorResponse {
         return ErrorResponse(GlobalResponseType.BAD_REQUEST, e.reason ?: "")
+    }
+
+
+    @ExceptionHandler(HttpMessageNotReadableException::class)
+    fun handleHttpMessageNotReadableException(e: HttpMessageNotReadableException): ErrorResponse {
+        val cause = e.cause as? MismatchedInputException
+            ?: return ErrorResponse(GlobalResponseType.BAD_REQUEST, "입력 값을 확인해주세요.")
+
+        val mismatchedFieldNames = cause.path.mapNotNull { it.fieldName }
+        if (mismatchedFieldNames.isNotEmpty()) {
+            return ErrorResponse(GlobalResponseType.BAD_REQUEST, "입력 값을 확인해주세요. [${mismatchedFieldNames[0]}]")
+        }
+
+        return ErrorResponse(GlobalResponseType.BAD_REQUEST, "입력 값을 확인해주세요.")
     }
 
     @ExceptionHandler(MethodArgumentNotValidException::class)
