54a4f174447cd806fb44fe5e4901fd597c51136c
JisuNa <kuu2002@naver.com>
2024-11-18 16:35:09 +0900

    eqCM-43 Add common response to getProductDetail

--- src/main/kotlin/com/eqcm/api/presentation/controller/ProductController.kt
+++ src/main/kotlin/com/eqcm/api/presentation/controller/ProductController.kt
@@ -1,6 +1,7 @@
 package com.eqcm.api.presentation.controller
 
 import com.eqcm.api.application.service.ProductService
+import com.eqcm.api.presentation.common.response.SingleResponse
 import com.eqcm.api.presentation.controller.response.ProductDetailResponse
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -11,17 +12,19 @@ import org.springframework.web.bind.annotation.RestController
 @RequestMapping("/products")
 class ProductController(private val productService: ProductService) {
     @GetMapping("/{productId}")
-    fun getProductById(@PathVariable productId: Long): ProductDetailResponse {
-        return productService.getProductDetail(productId).let {
-            ProductDetailResponse(
-                brand = it.brand,
-                category = it.category,
-                product = it.product,
-                shipping = ProductDetailResponse.Shipping(
-                    estimatedTime = "3 days",
-                    fee = 5000
+    fun getProductDetail(@PathVariable productId: Long): SingleResponse<ProductDetailResponse> {
+        return SingleResponse(
+            productService.getProductDetail(productId).let {
+                ProductDetailResponse(
+                    brand = it.brand,
+                    category = it.category,
+                    product = it.product,
+                    shipping = ProductDetailResponse.Shipping(
+                        estimatedTime = "3 days",
+                        fee = 5000
+                    )
                 )
-            )
-        }
+            }
+        )
     }
 }
