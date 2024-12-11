7ee9263c8002069a484aa8ac2c5c904789f16aa4
JisuNa <kuu2002@naver.com>
2024-12-11 09:37:35 +0900

    eqCM-47 Modify naver login callback

--- src/main/kotlin/com/eqcm/api/presentation/controller/LoginController.kt
+++ src/main/kotlin/com/eqcm/api/presentation/controller/LoginController.kt
@@ -5,6 +5,7 @@ import com.eqcm.api.domain.vo.AuthTokenVo
 import com.eqcm.api.presentation.common.response.SingleResponse
 import com.eqcm.api.presentation.controller.request.EmailLoginRequest
 import com.eqcm.api.presentation.controller.request.NaverLoginRequest
+import com.eqcm.api.presentation.controller.response.NaverLoginInfoResponse
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
@@ -22,9 +23,8 @@ class LoginController(private val loginService: LoginService) {
     }
 
     @GetMapping("/naver/callback", name = "네이버 로그인 콜백")
-    fun callBackNaverLogin(req: NaverLoginRequest, response: HttpServletResponse) {
-        val redirectUrl = "/login/naver/token?code=${req.code}&state=${req.state}"
-        response.sendRedirect(redirectUrl)
+    fun callBackNaverLogin(req: NaverLoginRequest, response: HttpServletResponse): NaverLoginInfoResponse {
+        return NaverLoginInfoResponse(req.code, req.state)
     }
 
     @GetMapping("/naver/token", name = "네이버 로그인 토큰")
---
+++ src/main/kotlin/com/eqcm/api/presentation/controller/response/NaverLoginInfoResponse.kt
@@ -0,0 +1,6 @@
+package com.eqcm.api.presentation.controller.response
+
+class NaverLoginInfoResponse(
+    val code: String,
+    val state: String
+)
